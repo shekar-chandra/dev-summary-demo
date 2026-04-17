5f84abfbb3a0a5434b80c4565de30148bc30579d
chandra sekhar <47810059+shekar-chandra@users.noreply.github.com>
2026-04-17 12:06:11 +0530

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,13 @@
-// Calculator.java
 public class Calculator {
+
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int sub(int a, int b) {
+        return a - b;
     }
 }
